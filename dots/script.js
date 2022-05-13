69f68211a6f6593c25e20985695c668eca17e638
Jimmy Mendez <jmen34956@gmail.com>
2022-05-13 16:27:46 -0400

    create page

---
+++ dots/script.js
@@ -0,0 +1,18 @@
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement("div")
+        row.className = "number-row"
+        
+        for (let j = 0; j < 6; j++) {
+            let dot = document.createElement("div")
+            dot.className = "dot"
+            row.appendChild(dot)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard();
