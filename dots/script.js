e0171f291477e09d26bf94b962d17d24d2868873
Jimmy Mendez <jmen34956@gmail.com>
2022-05-16 16:32:28 -0400

    fix bug

--- dots/script.js
+++ dots/script.js
@@ -25,49 +25,86 @@ initBoard();
 
 var elements = document.getElementsByClassName("dot");
 
+//function chooseDot(target){
+//    let dotNum=-1
+//    for (var i = 0; i < elements.length; i++) {
+//        if(target == elements[i]){
+//            dotNum=i
+//            }
+//        }
+//    if(!permanent.includes(dotNum){
+//        if (elements[dotNum].style.backgroundColor==currentPlayer){
+//            elements[dotNum].style.backgroundColor='white'
+//            move.pop()
+//            countShaded-=1
+//            lastShaded = -10
+//        }
+//        else if (countShaded==0 || dotNum == lastShaded+6 || dotNum == lastShaded-6 || dotNum == lastShaded+1 || dotNum == lastShaded-1){
+//            elements[dotNum].style.backgroundColor=currentPlayer
+//            lastShaded = dotNum
+//            countShaded+=1
+//            move.push(dotNum)
+//        }
+//        else {
+//            if(lastShaded!=-10){
+//                elements[lastShaded].style.backgroundColor='white'
+//                }
+//                elements[dotNum].style.backgroundColor=currentPlayer
+//                lastShaded=dotNum
+//                move.pop()
+//                move.push(dotNum)
+//            }
+//    }
+//    if(countShaded==2){
+//        permanent.concat(move)
+//        if(currentPlayer == "blue"){
+//            currentPlayer = "red"
+//        }
+//        else{
+//            currentPlayer = "blue"
+//        }
+//        countShaded=0
+//        lastShaded = -10
+//    }
+//}
+       
 function chooseDot(target){
-    let oldColor = target.style.backgroundColor
-    let dotNum=-1
-    for (var i = 0; i < elements.length; i++) {
-        if(target == elements[i]){
-            dotNum=i
-            }
-        }
-    if(!permanent.includes(dotNum){
-        if (elements[dotNum].style.backgroundColor==currentPlayer){
-            elements[dotNum].style.backgroundColor='white'
-            move.pop()
-            countShaded-=1
-            lastShaded = -10
-        }
-        else if (countShaded==0 || dotNum == lastShaded+6 || dotNum == lastShaded-6 || dotNum == lastShaded+1 || dotNum == lastShaded-1){
-            elements[dotNum].style.backgroundColor=currentPlayer
-            lastShaded = dotNum
-            countShaded+=1
-            move.push(dotNum)
-        }
-        else {
-            if(lastShaded!=-10){
-                elements[lastShaded].style.backgroundColor='white'
-                }
-                elements[dotNum].style.backgroundColor=currentPlayer
-                lastShaded=dotNum
-                move.pop()
-                move.push(dotNum)
-            }
-    }
-    if(countShaded==2){
-        permanent.concat(move)
-        if(currentPlayer == "blue"){
-            currentPlayer = "red"
-        }
-        else{
-            currentPlayer = "blue"
-        }
-        countShaded=0
-        lastShaded = -10
-    }
+   let dotNum=-1
+   for (var i = 0; i < elements.length; i++) {
+       if(target == elements[i]){
+           dotNum=i
+           }
+       }
+       if (elements[dotNum].style.backgroundColor=='black'){
+           elements[dotNum].style.backgroundColor='white'
+           move.pop()
+           countShaded-=1
+           lastShaded = -10
+       }
+       else if (countShaded==0 || dotNum == lastShaded+6 || dotNum == lastShaded-6 || dotNum == lastShaded+1 || dotNum == lastShaded-1){
+           elements[dotNum].style.backgroundColor='black'
+           lastShaded = dotNum
+           countShaded+=1
+           move.push(dotNum)
+       }
+       else {
+           if(lastShaded!=-10){
+               elements[lastShaded].style.backgroundColor='white'
+               }
+               elements[dotNum].style.backgroundColor='black'
+               lastShaded=dotNum
+               move.pop()
+               move.push(dotNum)
+           }
+   }
+   if(move.length==2){
+       elements[move[0]].style.backgroundColor='white'
+       elements[move[1]].style.backgroundColor='white'
+       console.log('draw the line')
+   }
 }
+       
+
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', (e) => {
