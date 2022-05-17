55b848fc2f96d10f31f0a9e0f42b2e62da206b79
Jimmy Mendez <jmen34956@gmail.com>
2022-05-17 01:28:49 -0400

    shading

--- dots/script.js
+++ dots/script.js
@@ -24,7 +24,45 @@ function initBoard() {
 initBoard();
 
 var elements = document.getElementsByClassName("dot");
-       
+    
+function adjustLine(from, to, line){
+
+    var fT = from.offsetTop  + from.offsetHeight/2;
+    var tT = to.offsetTop    + to.offsetHeight/2;
+    var fL = from.offsetLeft + from.offsetWidth/2;
+    var tL = to.offsetLeft   + to.offsetWidth/2;
+    
+    var CA   = Math.abs(tT - fT);
+    var CO   = Math.abs(tL - fL);
+    var H    = Math.sqrt(CA*CA + CO*CO);
+    var ANG  = 180 / Math.PI * Math.acos( CA/H );
+  
+    if(tT > fT){
+        var top  = (tT-fT)/2 + fT;
+    }else{
+        var top  = (fT-tT)/2 + tT;
+    }
+    if(tL > fL){
+        var left = (tL-fL)/2 + fL;
+    }else{
+        var left = (fL-tL)/2 + tL;
+    }
+  
+    if(( fT < tT && fL < tL) || ( tT < fT && tL < fL) || (fT > tT && fL > tL) || (tT > fT && tL > fL)){
+      ANG *= -1;
+    }
+    top-= H/2;
+  
+    line.style["-webkit-transform"] = 'rotate('+ ANG +'deg)';
+    line.style["-moz-transform"] = 'rotate('+ ANG +'deg)';
+    line.style["-ms-transform"] = 'rotate('+ ANG +'deg)';
+    line.style["-o-transform"] = 'rotate('+ ANG +'deg)';
+    line.style["-transform"] = 'rotate('+ ANG +'deg)';
+    line.style.top    = top+'px';
+    line.style.left   = left+'px';
+    line.style.height = H + 'px';
+  }
+
 function chooseDot(target){
    let dotNum=-1
    for (var i = 0; i < elements.length; i++) {
@@ -54,6 +92,11 @@ function chooseDot(target){
                move.push(dotNum)
            }
    if(countShaded==2){
+        adjustLine(
+            elements[move[0]], 
+            elements[move[1]],
+            document.getElementById('line')
+      );
        elements[move[0]].style.backgroundColor='white'
        elements[move[1]].style.backgroundColor='white'
        lines.push(move)
@@ -61,10 +104,9 @@ function chooseDot(target){
        countShaded = 0
        lastShaded=-10
        console.log(move)
-       console.log(line)
+       console.log(lines)
    }
 }
-       
 
 
 for (var i = 0; i < elements.length; i++) {
